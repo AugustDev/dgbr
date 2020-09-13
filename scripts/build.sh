8c1412601333b624c967be31fdf3fedf5e81daeb
Augustinas Malinauskas <augustinasmal@gmail.com>
2020-09-13 13:10:01 +0100

    build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/linux/dgbr .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -a -installsuffix cgo -o bin/macOS/dgbr .
