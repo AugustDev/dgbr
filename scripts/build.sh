14d5258beee3fdb9ecbdcfe712e58b7ebef91e0c
Augustinas Malinauskas <augustinasmal@gmail.com>
2020-09-21 18:55:18 +0100

    removed scripts tracking

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# compile
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/linux/dgbr .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -a -installsuffix cgo -o bin/macOS/dgbr .
-
-# gzip
-gzip -c bin/linux/dgbr > release/dgbr.gz && mv release/dgbr.gz release/dgbr-linux-amd64.gz
-gzip -c bin/macOS/dgbr > release/dgbr.gz && mv release/dgbr.gz release/dgbr-darwin-amd64.gz
